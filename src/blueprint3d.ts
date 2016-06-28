d65af6526c1db4d738513b944bc270eb20c95973
Ekkehard Beier <eb@intelligentgraphics.biz>
2016-06-28 16:31:48 +0200

    modularized version of blueprint3d

--- src/blueprint3d.ts
+++ src/blueprint3d.ts
@@ -2,17 +2,20 @@
 /// <reference path="model/model.ts" />
 /// <reference path="floorplanner/floorplanner.ts" />
 
-var Blueprint3d = function (opts) {
-  // opts.threeElement
-  // opts.floorplannerElement
-  // opts.textureDir
+module BP3D {
+  /** */
+  export var Blueprint3d = function (opts) {
+    // opts.threeElement
+    // opts.floorplannerElement
+    // opts.textureDir
 
-  this.model = new BP3D.Model.Model(opts.textureDir);
-  this.three = new BP3D.Three.Main(this.model, opts.threeElement, opts.threeCanvasElement, {});
-  if (!opts.widget) {
-    this.floorplanner = new BP3D.Floorplanner(opts.floorplannerElement, this.model.floorplan);
-  }
-  else {
-    this.three.getController().enabled = false;
+    this.model = new Model.Model(opts.textureDir);
+    this.three = new Three.Main(this.model, opts.threeElement, opts.threeCanvasElement, {});
+    if (!opts.widget) {
+      this.floorplanner = new Floorplanner(opts.floorplannerElement, this.model.floorplan);
+    }
+    else {
+      this.three.getController().enabled = false;
+    }
   }
 }
