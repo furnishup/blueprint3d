a5acde7fc768021e21c7ee65c769f93d24e4e71b
Ekkehard Beier <eb@intelligentgraphics.biz>
2016-06-28 16:22:45 +0200

    wrong insert char... (copy-paste issue)

--- src/blueprint3d.ts
+++ src/blueprint3d.ts
@@ -3,17 +3,16 @@
 /// <reference path="floorplanner/floorplanner.ts" />
 
 var Blueprint3d = function (opts) {
-	// opts.threeElement
-	// opts.floorplannerElement
-	// opts.textureDir
+  // opts.threeElement
+  // opts.floorplannerElement
+  // opts.textureDir
 
-	this.model = new BP3D.Model.Model(opts.textureDir);
-	this.three = new BP3D.Three.Main(this.model, opts.threeElement, opts.threeCanvasElement, {});
-	if (!opts.widget) {
-		this.floorplanner = new BP3D.Floorplanner(opts.floorplannerElement, this.model.floorplan);
-	}
-	else {
-		this.three.getController().enabled = false;
-	}
+  this.model = new BP3D.Model.Model(opts.textureDir);
+  this.three = new BP3D.Three.Main(this.model, opts.threeElement, opts.threeCanvasElement, {});
+  if (!opts.widget) {
+    this.floorplanner = new BP3D.Floorplanner(opts.floorplannerElement, this.model.floorplan);
+  }
+  else {
+    this.three.getController().enabled = false;
+  }
 }
-
